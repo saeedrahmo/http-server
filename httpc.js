811e0e5d413af5f1f43acd66355a64d2cf218213
Saeed Rahmani <saeedrahmo@gmail.com>
2021-10-22 22:05:43 -0400

    get is developed

---
+++ httpc.js
@@ -0,0 +1,292 @@
+var net = require("net");
+var yargs = require("yargs");
+var url = require("url");
+const fs = require("fs");
+
+var client = new net.Socket();
+
+var argv = yargs
+  .usage("Usage: $0 command [arguments]")
+  // examples based on https://httpbin.org/
+  .example(
+    'httpc get -v "http://httpbin.org/get?course=networking&assignment=1"'
+  )
+  .example(
+    "httpc post -h Content-Type:application/json -d {\"Assignment\":1} 'http://httpbin.org/post'"
+  )
+  .example(
+    'httpc post -h Content-Type:application/json -f "assignment.json" http://httpbin.org/post'
+  )
+  .example("httpc get http://httpbin.org/status/418")
+  .example("httpc get -i -v http://httpbin.org/status/301")
+  .command({
+    command: "post",
+    desc: "executes a HTTP POST request and prints the response.",
+    builder: (yargs) => {
+      yargs
+        .usage(
+          "usage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL"
+        )
+        .option("v", {
+          //alias: "verbose",
+          describe:
+            "Prints the detail of the response such as protocol, status, and headers.",
+          type: "boolean",
+        })
+        .option("h", {
+          //alias: "header",
+          type: "string",
+          describe:
+            "Associates headers to HTTP Request with the format 'key:value'.",
+        })
+        .option("d", {
+          //alias: "data",
+          type: "string",
+          describe: "Associates an inline data to the body HTTP POST request.",
+        })
+        .option("f", {
+          //alias: "file",
+          type: "string",
+          describe:
+            "Associates the content of a file to the body HTTP POST request.",
+        })
+        .option("o", {
+          //alias: "filename",
+          type: "string",
+          describe:
+            "Write the body of the response to the specified file instead of the console.",
+        });
+    },
+    handler: (argv) => {
+      if (argv._.length == 2) {
+        // if (isUrlValid(argv._[1])) {
+        console.log(argv);
+        var parsedUrl = url.parse(removeQuetes(argv._[1]));
+        client.connect(
+          parsedUrl.port ? parsedUrl.port : 80,
+          parsedUrl.hostname,
+          function () {
+            client.write(
+              `POST ${parsedUrl.path} HTTP/1.0\r\nHost:${parsedUrl.hostname}${
+                argv.h
+                  ? Array.isArray(argv.h)
+                    ? "\r\n" + argv.h.join("\r\n")
+                    : "\r\n" + argv.h.trim()
+                  : ""
+              }${
+                argv.d
+                  ? "\r\n" +
+                    "Content-Length:" +
+                    argv.d.length +
+                    "\r\n\r\n" +
+                    argv.d
+                  : ""
+              }${
+                argv.f
+                  ? "\r\n" +
+                    "Content-Length:" +
+                    JSON.stringify(JSON.parse(fs.readFileSync(argv.f, "utf-8")))
+                      .length +
+                    "\r\n\r\n" +
+                    JSON.stringify(JSON.parse(fs.readFileSync(argv.f, "utf-8")))
+                  : ""
+              }`
+            );
+          }
+        );
+        //}
+      }
+    },
+  })
+  .command(
+    "get",
+    "executes a HTTP GET request and prints the response.",
+    function (yargs) {
+      return yargs
+        .usage("usage: httpc get [-v] [-h key:value] URL")
+        .option("v", {
+          //alias: "verbose",
+          type: "boolean",
+          describe:
+            "Prints the detail of the response such as protocol, status, and headers.",
+        })
+        .option("h", {
+          //alias: "header",
+          type: "string",
+          describe:
+            "Associates headers to HTTP Request with the format 'key:value'.",
+        })
+        .option("o", {
+          //alias: "filename",
+          type: "string",
+          describe:
+            "Write the body of the response to the specified file instead of the console.",
+        })
+        .option("i", {
+          //alias: "redirect",
+          type: "bool",
+          describe:
+            "Redirect the request to the location header in case of statuse code 3xx.",
+        });
+    },
+    function (argv) {
+      if (argv._.length == 2) {
+        // console.log(isUrlValid(argv._[1]));
+        //if (isUrlValid(argv._[1])) {
+        var parsedUrl = url.parse(removeQuetes(argv._[1]));
+        console.log(`url: ${parsedUrl.hostname} port:${parsedUrl.port}`);
+        client.connect(
+          parsedUrl.port ? parsedUrl.port : 80,
+          parsedUrl.hostname,
+          function () {
+            client.write(
+              `GET ${parsedUrl.path} HTTP/1.0\r\nHost:${
+                parsedUrl.hostname
+              }\r\n${
+                argv.h
+                  ? Array.isArray(argv.h)
+                    ? argv.h
+                    : argv.h.join("\r\n")
+                  : ""
+              }\r\n`
+            );
+          }
+        );
+        // }
+      }
+    }
+  )
+  .updateStrings({
+    "Commands:": "The commands are:",
+    //"Options:": " ",
+  })
+  .version(false)
+  //.alias("h", "help")
+  .help("help")
+  .describe(
+    "help",
+    'Use "httpc [command] help" for more information about a command.'
+  ).argv;
+//.wrap(null)
+var client2 = new net.Socket();
+//yargs.showHelp();
+
+client.on("connect", function () {
+  // console.log(`<=== Connection connected ===>`);
+});
+
+client.on("timeout", function (time) {
+  console.log(`<=== Timeout: ${time} ===>`);
+});
+
+client.on("error", function (err) {
+  console.log(`<=== Error: ${err} ===>`);
+});
+
+client.on("data", function (data) {
+  var dt = argv.v
+    ? data.toString("utf8")
+    : data.toString("utf8").split("\r\n\r\n").pop();
+
+  if (!argv.o) {
+    console.log(dt);
+  } else {
+    fs.writeFile(argv.o, dt, function (err) {
+      if (err) {
+        return console.log(`<===Error: ${err}===>`);
+      }
+      console.log("<=== The file was saved ===>");
+    });
+  }
+
+  if (argv.i) {
+    var tx = data.toString("utf8").split("\r\n\r\n")[0];
+    var he = "http/1.1";
+    var redirect_url = "";
+    var is_redirect =
+      tx.substring(
+        tx.toLowerCase().indexOf(he) + 9,
+        tx.toLowerCase().indexOf(he) + 13
+      ) == 301;
+
+    if (is_redirect) {
+      var lo = "location:";
+      var ta = tx.split("\r\n");
+
+      for (var i = 0; i < ta.length; i++) {
+        if (ta[i].toLowerCase().indexOf(lo) > -1) {
+          redirect_url = ta[i].substring(
+            ta[i].toLowerCase().indexOf(lo) + 10,
+            ta[i].length
+          );
+        }
+      }
+    }
+
+    var parsedUrl = url.parse(redirect_url);
+    var host_name = "";
+    if (parsedUrl.hostname) {
+      host_name = parsedUrl.hostname;
+    } else {
+      if (argv._.length == 2)
+        if (isUrlValid(argv._[1]))
+          host_name = url.parse(removeQuetes(argv._[1])).hostname;
+    }
+
+    redirect_func(host_name, redirect_url);
+  }
+});
+
+var redirect_func = (host_name, redirect_url) => {
+  client2.connect(80, host_name, function () {
+    client2.write(`GET ${redirect_url} HTTP/1.0\r\nHost: ${host_name}\r\n\r\n`);
+  });
+};
+
+client2.on("data", function (data) {
+  var dt = argv.v
+    ? data.toString("utf8")
+    : data.toString("utf8").split("\r\n\r\n").pop();
+
+  if (!argv.o) {
+    console.log(dt);
+  } else {
+    fs.stat(argv.o, function (err, stat) {
+      if (err == null) {
+        //Exist
+        fs.appendFile(argv.o, dt, function (err) {
+          if (err) return console.log(`<===Error: ${err}===>`);
+          console.log("<=== The file was saved ===>");
+        });
+      } else if (err.code == "ENOENT") {
+        // NO exist
+        fs.writeFile(argv.o, dt, function (err) {
+          if (err) {
+            return console.log(`<===Error: ${err}===>`);
+          }
+          console.log("<=== The file was saved ===>");
+        });
+      }
+    });
+  }
+});
+
+client2.on("close", function () {
+  //console.log(`<=== Connection closed ===>`);
+});
+
+client.on("close", function () {
+  //console.log(`<=== Connection closed ===>`);
+});
+
+function isUrlValid(userInput) {
+  var res = userInput.match(
+    /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+  );
+  if (res == null) return false;
+  else return true;
+}
+
+function removeQuetes(str) {
+  return str.replace(/['"]+/g, "");
+}
---
+++ httpfs.js
@@ -0,0 +1,231 @@
+const net = require("net");
+const yargs = require("yargs");
+const path = require("path");
+const fs = require("fs");
+
+const testFolder = "./tests/";
+
+const argv = yargs
+  .usage("node echoserver.js [--port port]")
+  .option("v", {
+    alias: "verbose",
+    describe: "Prints debugging messages.",
+    type: "boolean",
+  })
+  .option("p", {
+    alias: "port",
+    describe:
+      "Specifies the port number that the server will listen and serve at.",
+    type: "number",
+    default: 8080,
+  })
+  .option("d", {
+    alias: "dir",
+    describe:
+      "Specifies the directory that the server will use to read/write requested files. Default is the current directory when launching the application.",
+    type: "string",
+    default: "/home",
+  })
+  .version(false)
+  .help("help").argv;
+
+const server = net.createServer(handleClient).on("error", (err) => {
+  throw err;
+});
+
+// TODO: family: "IPv4", address: "127.0.0.1"
+server.listen({ port: argv.port }, () => {
+  console.log(argv);
+  console.log("Echo server is listening at %j", server.address());
+});
+
+// HTTP/1.1 200 OK
+// Date: Fri, 22 Oct 2021 18:54:24 GMT
+// Content-Type: application/json
+// Content-Length: 284
+// Connection: close
+// Server: gunicorn/19.9.0
+// Access-Control-Allow-Origin: *
+// Access-Control-Allow-Credentials: true
+
+function readFiles(dir, processFile) {
+  // read directory
+  fs.readdir(dir, (error, fileNames) => {
+    if (error) throw error;
+
+    fileNames.forEach((filename) => {
+      // get current file name
+      const name = path.parse(filename).name;
+      // get current file extension
+      const ext = path.parse(filename).ext;
+      // get current file path
+      const filepath = path.resolve(dir, filename);
+
+      // get information about the file
+      fs.stat(filepath, function (error, stat) {
+        if (error) throw error;
+
+        // check if the current path is a file or a folder
+        const isFile = stat.isFile();
+
+        // exclude folders
+        if (isFile) {
+          // callback, do something with the file
+          processFile(filepath, name, ext, stat);
+        }
+      });
+    });
+  });
+}
+
+/**
+ * @description Read files synchronously from a folder, with natural sorting
+ * @param {String} dir Absolute path to directory
+ * @returns {Object[]} List of object, each object represent a file
+ * structured like so: `{ filepath, name, ext, stat }`
+ */
+function readFilesSync(dir) {
+  const files = [];
+
+  fs.readdirSync(dir).forEach((filename) => {
+    const name = path.parse(filename).name;
+    const ext = path.parse(filename).ext;
+    const filepath = path.resolve(dir, filename);
+    const stat = fs.statSync(filepath);
+    const isFile = stat.isFile();
+
+    if (isFile) files.push(name);
+    // files.push({ filepath, name, ext, stat });
+  });
+
+  // files.sort((a, b) => {
+  //   // natural sort alphanumeric strings
+  //   // https://stackoverflow.com/a/38641281
+  //   return a.name.localeCompare(b.name, undefined, {
+  //     numeric: true,
+  //     sensitivity: "base",
+  //   });
+  // });
+
+  return files;
+}
+
+function handleClient(socket) {
+  console.log("New client from %j", socket.address());
+  socket
+    .on("data", (buf) => {
+      var req = buf.toString("utf-8");
+      var reqSplit = req.split("\r\n");
+      var reqType = reqSplit[0].split(/\s+/)[0];
+      var reqAddress = reqSplit[0].split(/\s+/)[1];
+      var homeDir = "." + argv.d;
+
+      if (reqType.toLowerCase() == "get") {
+        if (reqAddress == "/") {
+          const filesList = readFilesSync(path.resolve(__dirname, homeDir));
+          filesObj = new Object();
+          filesObj["files_list"] = filesList;
+          const jsonFiles = JSON.stringify(filesObj);
+
+          const contentLength = jsonFiles.length;
+          const httpCode = "200 OK";
+          const serverDate = new Date().toString();
+          const serverVersion = `Node.js/${process.version}`;
+          var response = `HTTP/1.1 ${httpCode}\nDate: ${serverDate}\nContent-Type: application/json\nContent-Length: ${contentLength}\nConnection: close\nServer: ${serverVersion}\n\n${jsonFiles}`;
+
+          socket.write(response);
+        } else {
+          const reqFileName = reqAddress.substring(1);
+
+          const allFiles = fs.readdirSync(path.resolve(__dirname, homeDir));
+          for (const item of allFiles) {
+            const extname = path.extname(item);
+            const filename = path.basename(item, extname);
+            if (reqFileName == filename) {
+              console.log(item);
+            }
+          }
+        }
+      }
+
+      reqBody = req.split("\r\n\r\n")[1];
+
+      // console.log(reqSplit[0].split(/\s+/)[2]);
+      // console.log(reqSplit[1]);
+      // console.log(reqSplit[2]);
+      // console.log(reqSplit[3]);
+      // console.log(buf.toString("utf-8"));
+
+      //path.resolve(__dirname, folder)
+      //var filesList = [];
+      //var fname = "";
+      // fs.readdir(__dirname, (err, files) => {
+      //   if (err) throw err;
+
+      //   for (let file of files) {
+      //     const extname = path.extname(file);
+      //     const filename = path.basename(file, extname);
+      //     //const absolutePath = path.resolve(folder, file);
+      //     filesList.push(filename);
+      //     fname += filename;
+      //     //console.log(filename);
+      //   }
+      // });
+
+      // fs.readdir(__dirname, (err, files) => {
+      //   files.forEach((file) => {
+      //     const extname = path.extname(file);
+      //     const filename = path.basename(file, extname);
+      //     // console.log(filename);
+
+      //     filesList.push(filename);
+      //     fname += filename;
+      //   });
+      // });
+
+      // use an absolute path to the folder where files are located
+      // readFiles(__dirname, (filepath, name, ext, stat) => {
+      //   // console.log("file path:", filepath);
+      //   filesList.push(name);
+      //   //console.log("file name:", name);
+      //   //console.log("file extension:", ext);
+      //   //console.log("file information:", stat);
+      // });
+
+      // return an array list of objects
+      // each object represent a file
+
+      //console.log(`FILESLIST: ${files}`);
+      //console.log(`FNAME: ${fname}`);
+
+      // console.log("Current directory:", __dirname);
+      // console.log("Current directory:", path.dirname(__filename));
+
+      // const event = new Date();
+      // const options = { year: "numeric", month: "short", day: "short" };
+
+      // console.log(event.toLocaleDateString("de-DE", options));
+      // expected output: Donnerstag, 20. Dezember 2012
+      // Date: Fri, 22 Oct 2021 18:38:24 GMT
+      // console.log(event.toLocaleDateString("en-US", options));
+      // console.log(new Date(new Date().toUTCString()));
+      ///  console.log(new Date(new Date().toGMTString()));
+      // US format
+
+      //console.log(new Date().toISOString());
+
+      // socket.write(
+      //   "HTTP/1.1 ${}\n" +
+      //     "Content-Type: text/html\n" +
+      //     "\n" +
+      //     "<html><body>Hello World</body></html>\n"
+      // );
+    })
+    .on("error", (err) => {
+      console.log("socket error %j", err);
+      socket.destroy();
+    })
+    .on("end", () => {
+      socket.destroy();
+    });
+}
